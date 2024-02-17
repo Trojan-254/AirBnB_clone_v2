089fe2afdce263f1436106dbc5a9b6776fb3a7e7
Samwuel Simiyu <samwuels254@gmail.com>
2024-02-17 12:40:48 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
+"""
+Starts a flask web application
+"""
 
 from flask import Flask
-
 app = Flask(__name__)
-app.config['STRICT_SLASHES'] = False
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def helloHBNB():
     """displays Hello HBNB!"""
     return "Hello HBNB!"
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
