ad1acc9faa6d1c3c039d3f9778d899c6cf572bf1
Samwuel simiyu <samwuels254@gmail.com>
2024-02-17 09:36:04 +0000

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+app = Flask(__name__)
+app.config['STRICT_SLASHES'] = False
+
+@app.route('/')
+def helloHBNB():
+    """displays Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hello():
+   """displays hbnb"""
+   return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    
