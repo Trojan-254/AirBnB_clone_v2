ebdfd6294a0ada865edb36c968106436353046ec
Samwuel Simiyu <samwuels254@gmail.com>
2024-03-25 11:38:12 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -25,7 +25,7 @@ class State(BaseModel, Base):
         """initializes state"""
         super().__init__(*args, **kwargs)
 
-   if models.storage_t != "db":
+    if models.storage_t != "db":
         @property
         def cities(self):
             """getter for list of city instances related to the state"""
